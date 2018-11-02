fe6898f1bfca1169e9a078f7781344eb61d8248e
Ralakus <12schneide@gmail.com>
2018-11-02 19:31:16 -0400

    Seperated tokens to tokens.h

--- src/lexer.h
+++ src/lexer.h
@@ -2,51 +2,7 @@
 
 #include <logger.h>
 #include <lexer.h>
-
-typedef enum tokens_e {
-    tok_nil,
-
-    tok_whitespace_space,
-    tok_whitespace_tab,
-    tok_whitespace_return,
-    tok_whitespace_newline,
-
-    tok_identifier,
-    tok_number,
-
-    tok_char,
-    tok_string,
-
-    tok_lparen,
-    tok_rparen,
-    tok_lbracket,
-    tok_rbracket,
-    tok_lcurley,
-    tok_rcurley,
-
-    tok_comma,
-    tok_colon,
-    tok_semicolon,
-
-    tok_func,
-    tok_let,
-    tok_return,
-
-    tok_operator_plus,
-    tok_operator_minus,
-    tok_operator_mul,
-    tok_operator_div,
-    tok_operator_equals,
-    tok_operator_xor,
-    tok_operator_and,
-    tok_operator_lesst,
-    tok_operator_greatert,
-    tok_operator_or,
-    tok_operator_not,
-
-    tok_eof,
-
-}   tokens_e;
+#include "tokens.h"
 
 extern char* tok_to_string(tokens_e tok);
 
