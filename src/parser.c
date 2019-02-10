2285c4b8b53fdf4370ca72ecf7747b03e9617d66
Ralakus <12schneide@gmail.com>
2019-02-10 10:48:26 -0500

    Parser start

--- src/parser.c
+++ src/parser.c
@@ -1,2 +1,12 @@
 #include "parser.h"
 
+void lab_parser_init(lab_parser_t* parser) {
+    parser->container = NULL;
+    parser->current = NULL;
+    parser->index = 0;
+    parser->bytecode = NULL;
+}
+
+void lab_parser_free(lab_parser_t* parser) {
+
+}
--- src/parser.h
+++ src/parser.h
@@ -1,2 +1,14 @@
 #pragma once
 
+#include "lexer.h"
+#include "vm.h"
+
+typedef struct lab_parser {
+    lab_lexer_token_container_t* container;
+    lab_tokens_e_t* current;
+    size_t index;
+    lab_vm_bytecode_t* bytecode;
+} lab_parser_t;
+
+void lab_parser_init(lab_parser_t* parser);
+void lab_parser_free(lab_parser_t* parser);
