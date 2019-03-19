5d9f1f8fa1418ed6299b047dad21ff2b6c3cc127
Ralakus <12schneide@gmail.com>
2019-03-18 22:56:48 -0400

    Very primitive argument parsing

--- src/main.c
+++ src/main.c
@@ -10,109 +10,234 @@
 #include <time.h>
 #include <stdlib.h>
 
-int main(int argc, const char* argv[]) {
+#include <getopt.h>
 
-    clock_t start, end;
-    double lex_time = 0.0, parse_time = 0.0, run_time = 0.0;
+int main(int argc, char* const argv[]) {
 
-    lab_lexer_token_container_t tokens;
-    lab_parser_t parser;
-    lab_vm_bytecode_t bytecode;
+    int opt = 0;
 
-    lab_vm_t vm;
-    lab_vm_init(&vm);
+    /*lab_vec_t file_names;
+    lab_vec_init(&file_names, sizeof(char*), 8);*/
 
-    /*char* test_file = NULL;
+    char* file_name = NULL;
+    char* output_name = NULL;
+    bool bytecode_flag = false;
+    bool compile_flag = false;
 
-    FILE* file = fopen("../test/test1mil.lab", "r");
-    if(file == NULL){
-        lab_errorln("Failed to open test file!");
-        return EXIT_FAILURE;
-    }
+    bool debug_prints = false;
 
-    size_t file_size = 0;
+    while((opt = getopt(argc, argv, "f:b:co:d")) != -1) {
+        switch(opt) {
 
-    fseek(file, 0, SEEK_END);
-    file_size = ftell(file) + 1;
-    rewind(file);
-
-    test_file = malloc(file_size);
-    if(test_file == NULL) {
-        lab_errorln("Failed to allocate!");
-        return EXIT_FAILURE;
-    }
-
-    if(fread(test_file, 1, file_size, file) < file_size - 1) {
-        lab_errorln("Failed to read!");
-        return EXIT_FAILURE;
-    }
-
-    test_file[file_size - 1] = '\0';
-
-    fclose(file);*/
-
-    char line[65536];
-    for(;;) {
-        lab_print("> ");
-        if (!fgets(line, sizeof(line), stdin)) {
-            lab_print_raw("\n");
-            break;
-        }
-        lab_lexer_token_container_init(&tokens, 16);
-        start = clock();
-        lab_lexer_lex(&tokens, line);
-        if(((lab_lexer_token_t*)lab_vec_at(&tokens.tokens, 0))->type == LAB_TOK_IDENTIFIER) {
-            if(memcmp(((lab_lexer_token_t*)lab_vec_at(&tokens.tokens, 0))->data, "exit", min(4, ((lab_lexer_token_t*)lab_vec_at(&tokens.tokens, 0))->data_len))==0) {
-                lab_lexer_token_container_free(&tokens);
+            case 'd':
+                lab_println("Debug mode enabled");
+                debug_prints = true;
                 break;
-            }
+
+            case 'c':
+                lab_println("Compiling source files");
+                compile_flag = true;
+                break;
+
+            case 'f':
+                lab_println("Source file: %s", optarg);
+                file_name = malloc(strlen(optarg) + 1);
+                memcpy(file_name, optarg, strlen(optarg) + 1);
+                bytecode_flag = false;
+                break;
+
+            case 'b':
+                lab_println("Bytecode file: %s", optarg);
+                file_name = malloc(strlen(optarg) + 1);
+                memcpy(file_name, optarg, strlen(optarg) + 1);
+                bytecode_flag = true;
+                break;
+
+            case 'o': 
+                lab_println("Outputing to file: %s", optarg);
+                output_name = malloc(strlen(optarg) + 1);
+                memcpy(output_name, optarg, strlen(optarg) + 1);
+                break;
+
+            case '?': 
+                lab_errorln("Unknown option %c", optopt);
+                free(file_name);
+                free(output_name);
+                return EXIT_FAILURE;
+
+            default:
+                break;
         }
-        end = clock();
+    }
 
-        lex_time = ((double)(end - start)) / CLOCKS_PER_SEC;
+    if(file_name==NULL) {
+        lab_errorln("No input files!");
+        free(file_name);
+        free(output_name);
+        return EXIT_FAILURE;
+    }
 
-        lab_lexer_token_container_print(&tokens);
+    if(compile_flag) {
+        if(bytecode_flag) {
+            lab_errorln("Cannot compile bytecode!");
+            free(file_name);
+            free(output_name);
+            return EXIT_FAILURE;
+        }
+        if(output_name==NULL) {
+            lab_errorln("Needs output file '-o'!");
+            free(file_name);
+            free(output_name);
+            return EXIT_FAILURE;
+        }
+    }
 
-        start = clock();
+    if(compile_flag) {
+
+        char* file_contents = NULL;
+
+        FILE* file = fopen(file_name, "r");
+        if(file == NULL){
+            lab_errorln("Failed to open '%s'!", file_name);
+            free(file_name);
+            free(output_name);
+            return EXIT_FAILURE;
+        }
+
+        size_t file_size = 0;
+
+        fseek(file, 0, SEEK_END);
+        file_size = ftell(file) + 1;
+        rewind(file);
+
+        file_contents = malloc(file_size);
+        if(file_contents == NULL) {
+            lab_errorln("Failed to allocate buffer for '%s'!", file_name);
+            free(file_name);
+            free(output_name);
+            return EXIT_FAILURE;
+        }
+
+        if(fread(file_contents, 1, file_size, file) < file_size - 1) {
+            lab_errorln("Failed to read '%s'!", file_name);
+            free(file_name);
+            free(output_name);
+            return EXIT_FAILURE;
+        }
+
+        file_contents[file_size - 1] = '\0';
+
+        fclose(file);
+
+        lab_lexer_token_container_t tokens;
+        lab_parser_t parser;
+        lab_vm_bytecode_t bytecode;
+
+        lab_lexer_token_container_init(&tokens, 16);
+
+        lab_lexer_lex(&tokens, file_contents);
+
+        if(debug_prints) lab_lexer_token_container_print(&tokens);
+
+        lab_vm_bytecode_init(&bytecode, 16);
+        lab_parser_init(&parser);
+
+        lab_parser_parse(&parser, &tokens, &bytecode);
+
+        size_t serialized_len = 0;
+        uint8_t* serialized = lab_vm_bytecode_serialize(&bytecode, &serialized_len, true);
+
+        if(debug_prints) lab_vm_bytecode_dissassemble(&bytecode, file_name);
+
+        if(debug_prints) {
+            lab_notice("Serialized ");
+            for(size_t i = 24; i < serialized_len; i++) {
+                lab_print_raw("%02hhx ", serialized[i]);
+            }
+            lab_println_raw("");
+        }
+
+
+        FILE* out = fopen(output_name, "wb+");
+        if(out == NULL){
+            lab_errorln("Failed to open '%s'!", output_name);
+            free(file_name);
+            free(output_name);
+            return EXIT_FAILURE;
+        }
+
+        fwrite(serialized, serialized_len, 1, out);
+
+        fclose(file);
+
+    }
+
+    else if(!bytecode_flag) {
+
+
+
+        char* file_contents = NULL;
+
+        FILE* file = fopen(file_name, "r");
+        if(file == NULL){
+            lab_errorln("Failed to open '%s'!", file_name);
+            free(file_name);
+            free(output_name);
+            return EXIT_FAILURE;
+        }
+
+        size_t file_size = 0;
+
+        fseek(file, 0, SEEK_END);
+        file_size = ftell(file) + 1;
+        rewind(file);
+
+        file_contents = malloc(file_size);
+        if(file_contents == NULL) {
+            lab_errorln("Failed to allocate buffer for '%s'!", file_name);
+            free(file_name);
+            free(output_name);
+            return EXIT_FAILURE;
+        }
+
+        if(fread(file_contents, 1, file_size, file) < file_size - 1) {
+            lab_errorln("Failed to read '%s'!", file_name);
+            free(file_name);
+            free(output_name);
+            return EXIT_FAILURE;
+        }
+
+        file_contents[file_size - 1] = '\0';
+
+        fclose(file);
+
+        lab_lexer_token_container_t tokens;
+        lab_parser_t parser;
+        lab_vm_bytecode_t bytecode;
+
+        lab_vm_t vm;
+        lab_vm_init(&vm);
+
+        lab_lexer_token_container_init(&tokens, 16);
+
+        lab_lexer_lex(&tokens, file_contents);
+
+        if(debug_prints) lab_lexer_token_container_print(&tokens);
 
         lab_parser_init(&parser);
         lab_vm_bytecode_init(&bytecode, 16);
 
         lab_parser_parse(&parser, &tokens, &bytecode);
 
-        size_t serialized_len = 0;
-        uint8_t* serialized = lab_vm_bytecode_serialize(&bytecode, &serialized_len, false);
 
-        lab_notice("Serialized ");
-        for(size_t i = 6; i < serialized_len; i++) {
-            lab_print_raw("%02hhx ", serialized[i]);
-        }
-        lab_println_raw("");
-
-        lab_vm_bytecode_deserialize(&bytecode, serialized);
-
-        lab_noticeln("Deserialzed");
-
-        free(serialized);
-
-        end = clock();
-
-        parse_time = ((double)(end - start)) / CLOCKS_PER_SEC;
-
-        lab_vm_bytecode_dissassemble(&bytecode, "Bytecode");
+        if(debug_prints) lab_vm_bytecode_dissassemble(&bytecode, "Bytecode");
 
         if(!parser.was_error) {
-            lab_noticeln(LAB_ANSI_COLOR_CYAN"--== Virtual Machine Stack Trace ==--"LAB_ANSI_COLOR_RESET);
+            if(debug_prints) lab_noticeln(LAB_ANSI_COLOR_CYAN"--== Virtual Machine Stack Trace ==--"LAB_ANSI_COLOR_RESET);
 
-            start = clock();
-
-            if(lab_vm_interpret_bytecode(&vm, &bytecode, true) != LAB_VM_INTERPRET_RESULT_SUCCESS) {
+            if(lab_vm_interpret_bytecode(&vm, &bytecode, debug_prints) != LAB_VM_INTERPRET_RESULT_SUCCESS) {
                 lab_errorln("Failed to execute bytecode!");
             }
-
-            end = clock();
-
-            run_time = ((double)(end - start)) / CLOCKS_PER_SEC;
 
         } else {
             lab_warnln("There was an error in compiling the input thus it will not be ran on the Virtual Machine");
@@ -122,11 +247,65 @@ int main(int argc, const char* argv[]) {
         lab_lexer_token_container_free(&tokens);
         lab_vm_bytecode_free(&bytecode);
 
-        lab_successln("Lex time: %fms, Parse time: %fms, Run time: %fms", lex_time * 1000, parse_time * 1000, run_time * 1000);
+        lab_vm_free(&vm);
 
     }
 
-    lab_vm_free(&vm);
+
+    else if(bytecode_flag) {
+
+        uint8_t* file_contents = NULL;
+
+        FILE* file = fopen(file_name, "rb");
+        if(file == NULL){
+            lab_errorln("Failed to open '%s'!", file_name);
+            free(file_name);
+            free(output_name);
+            return EXIT_FAILURE;
+        }
+
+        size_t file_size = 0;
+
+        fseek(file, 0, SEEK_END);
+        file_size = ftell(file);
+        rewind(file);
+
+        file_contents = malloc(file_size);
+        if(file_contents == NULL) {
+            lab_errorln("Failed to allocate buffer for '%s'!", file_name);
+            free(file_name);
+            free(output_name);
+            return EXIT_FAILURE;
+        }
+
+        if(fread(file_contents, 1, file_size, file) < file_size - 1) {
+            lab_errorln("Failed to read '%s'!", file_name);
+            free(file_name);
+            free(output_name);
+            return EXIT_FAILURE;
+        }
+
+        fclose(file);
+
+        lab_vm_t vm;
+        lab_vm_init(&vm);
+
+        lab_vm_bytecode_t bytecode;
+        lab_vm_bytecode_init(&bytecode, 0);
+        lab_vm_bytecode_deserialize(&bytecode, file_contents);
+
+        if(debug_prints) lab_vm_bytecode_dissassemble(&bytecode, file_name);
+
+        if(debug_prints) lab_noticeln(LAB_ANSI_COLOR_CYAN"--== Virtual Machine Stack Trace ==--"LAB_ANSI_COLOR_RESET);
+
+        if(lab_vm_interpret_bytecode(&vm, &bytecode, debug_prints) != LAB_VM_INTERPRET_RESULT_SUCCESS) {
+            lab_errorln("Failed to execute bytecode!");
+        }
+
+        lab_vm_bytecode_free(&bytecode);
+        lab_vm_free(&vm);
+
+    }
 
     return 0;
 
