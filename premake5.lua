4297bfd10b9cf628e70ad75b9225d097d089b1ae
Ralakus <12schneide@gmail.com>
2019-04-18 20:18:38 -0400

    Added build to library support

--- premake5.lua
+++ premake5.lua
@@ -79,3 +79,40 @@ project "lab"
 
     libdirs { "lib/LabyriCUtils/build/bin/" }
     links { "labcutils" }
+
+
+project "labyri" -- Static library for embedding
+    kind "StaticLib"
+
+    includedirs "src"
+    includedirs "lib/LabyriCUtils/src"
+
+    files {
+        "src/lexer.c",
+        "src/parser.c",
+        "src/vm.c",
+        "src/repl.c",
+        "src/utilities.c",
+    }
+
+    if is_64_bit then
+        defines { "LAB_64_BIT" }
+    else 
+        defines { "LAB_32_BIT" }
+    end
+
+    filter "configurations:Debug"
+        defines { "DEBUG" }
+
+        if build_os == linux and coverage_test then 
+            buildoptions { "-fprofile-arcs", "-ftest-coverage" }
+            linkoptions  { "-fprofile-arcs", "-ftest-coverage" }
+        end
+
+    filter "configurations:Release"
+        defines { "NDEBUG" }
+
+    filter {}
+
+    libdirs { "lib/LabyriCUtils/build/bin/" }
+    links { "labcutils" }
